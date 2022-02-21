2dc8d3f03eb6b9dfff67f0973e0989a9c6280c79
raisa khaliulina <evadar31@gmail.com>
2022-02-22 01:22:29 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const yargs = require("yargs");
+const pkg = require("./package.json");
+const { addNote, printNotes, removeNote, editNote } = require("./notes.controller")
+
+yargs.version(pkg.version)
+
+yargs.command({
+  command: "add",
+  describe: "Add new note to list",
+  builder: {
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true
+    }
+  },
+  handler({ title }) {
+    addNote(title)
+  }
+})
+
+yargs.command({
+  command: "edit",
+  describe: "Edit note by id",
+  builder: {
+    id: {
+      type: "string",
+      describe: "Note id",
+      demandOption: true,
+    },
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true,
+    },
+  },
+  async handler({ id, title }) {
+    await editNote(id, title);
+  },
+});
+
+
+yargs.command({
+  command: "list",
+  describe: "Print all notes",
+  async handler() {
+    printNotes({id, title})
+  }
+})
+
+yargs.remove({
+  command: "delete",
+  describe: "Remove note by id",
+   builder: {
+    id: {
+      type: "string",
+      describe: "Delete id",
+      demandOption: true,
+    },
+  },
+  async handler({id}) {
+    await removeNote(id);
+  }
+});
+
+yargs.parse()
---
+++ notes.controller.js
@@ -0,0 +1,62 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  const notes = await getNotes();
+  const note = { title, id: Date.now().toString() };
+
+  notes.push(note);
+  await saveNotes(notes);
+  console.log(chalk.green.inverse("Note was added!"));
+}
+
+async function removeNote(id) {
+  const notes = await getNotes();
+  const filtered = notes.filter((note) => note.id !== id);
+
+  await saveNotes(filtered);
+  console.log(chalk.red(`note with id: ${id} removed!`));
+}
+
+async function saveNotes(notes) {
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function editNote(id, title) {
+  const notes = await getNotes();
+  const updatedNotes = notes.map((note) => {
+    if (note.id === id) {
+      return { ...note, title: title };
+    } else {
+      return note;
+    }
+  });
+
+  await saveNotes(updatedNotes);
+  console.log(chalk.green.inverse(`Note id:${id} updated!`));
+}
+
+async function printNotes() {
+  const notes = await getNotes();
+
+  console.log(chalk.bgGray("List notes:"));
+  notes.forEach((note) => {
+    console.log(`${chalk.green(`id: ${note.id}`)} - ${chalk.blue(note.title)}`);
+  });
+}
+
+module.exports = {
+  addNote,
+  removeNote,
+  printNotes,
+  editNote,
+};
